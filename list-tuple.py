48cdf6c72170827e4f427e850854d98044f44d68
DENNISWUPC\ccdnd <dennis.wu@ccdntech.com>
2024-07-31 14:18:38 +0800

    list-tuple 練習

---
+++ list-tuple.py
@@ -0,0 +1,15 @@
+# 有序可以變動列表 List
+# grades=[11,60,15,70,99]
+# # grades[0]=55 
+# # grades[0:4] = [] #刪除
+# grades+=[77,81]
+# print(grades)
+
+# print(len(grades)) #取的列表長度
+data=[[1,2,3],[4,5,6]]
+print(data[0][1])
+
+
+# 有序不可變動列表 Tuple
+data=(3,5,8)
+print(data)
