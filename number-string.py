6967894490e3ab91e874c453d978d5857dbf6fba
DENNISWUPC\ccdnd <dennis.wu@ccdntech.com>
2024-07-31 13:40:58 +0800

    運算

---
+++ number-string.py
@@ -0,0 +1,58 @@
+#基本加點減法 
+x=3+4
+print(x)
+x=5-4
+print(x)
+x=5*4
+print(x)
+#除法說明 / 小數除法  // 整數除法
+y=8/5
+print(y)
+y= 51 // 10
+print(y)
+
+# 乘法 ** **0.5開根號
+x=3**2
+print(x)
+x=9**0.5
+print(x)
+
+#取餘數 %
+x=7%3
+print(x)
+
+x=x+1
+print(x)
+
+x+=1
+print(x)
+
+
+#字串運算
+
+s="Hell\"o" + "World" "hi"
+print(s)
+
+s="Hello\nWorld"
+print(s)
+
+s="""Hello
+World"""
+print(s)
+
+s="Hello"*3+"World"
+print(s)
+
+#字串匯兌內部的字元編號，從0開始
+
+s="Hello"
+print(s[0])
+
+s="Hello"
+print(s[1:3])
+
+s="Hello"
+print(s[1:])
+
+s="Hello"
+print(s[:4])
