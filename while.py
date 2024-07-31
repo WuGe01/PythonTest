9245f3fbcbb6b5931ea3d6d23ac5ede9cfd86ad1
DENNISWUPC\ccdnd <dennis.wu@ccdntech.com>
2024-07-31 17:37:29 +0800

    迴圈學習

---
+++ while.py
@@ -0,0 +1,26 @@
+# n=1
+# while n < 5:
+#     print("變數n的資料是：",n)
+#     n+=1
+# n=1
+# sum=0
+# while n <= 10:
+#     sum+=n
+#     n+=1
+# print(sum)
+
+# for x in [1,2,3,4,5,6,7]:
+#     print("x為：",x)
+
+# for x in "hello":
+#     print("x為：",x)
+
+# for x in range(7):
+#     print("x為：",x)
+
+# for x in range(3,6): #由三開始不包含六
+#     print("x為：",x)
+sum=0
+for x in range(1,11):
+    sum+=x
+print(sum)
